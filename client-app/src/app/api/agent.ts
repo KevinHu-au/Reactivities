eef039c3435210fb94849332a66bd8858d236cfe
Kevin Hu <kevin.hu@purple.telstra.com>
2022-05-28 19:51:21 +1000

    Add client side error handling

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,45 +1,81 @@
-import axios, { AxiosResponse } from "axios";
-import { Activity } from "../models/activity";
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { Activity } from '../models/activity';
+import { history } from '../..';
+import { toast } from 'react-toastify';
+import { store } from '../stores/store';
 
 const sleep = (delay: number) => {
-  return new Promise((_) => {
-    setTimeout(_, delay);
-  });
+	return new Promise((_) => {
+		setTimeout(_, delay);
+	});
 };
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = 'http://localhost:5000/api';
 
-axios.interceptors.response.use(async (response) => {
-  try {
-    await sleep(1000);
-    return response;
-  } catch (error) {
-    console.log(error);
-    return await Promise.reject(error);
-  }
-});
+axios.interceptors.response.use(
+	async (response) => {
+		await sleep(1000);
+		return response;
+	},
+	(error: AxiosError) => handleAxiosError(error)
+);
+
+const handleAxiosError = (error: AxiosError) => {
+  console.log('Error.response from Axios interceptors');
+	const { data, status, config } = error.response!;
+  console.log(error.response!);
+	switch (status) {
+		case 400:
+			if (config.method === 'get' && data.errors?.hasOwnProperty('id')) {
+				history.push('/not-found');
+			}
+
+			if (data.errors) {
+				const modalStateErrors = [];
+				for (const key in data.errors) {
+					if (data.errors[key]) {
+						modalStateErrors.push(data.errors[key]);
+					}
+				}
+				throw modalStateErrors.flat();
+			} else {
+				toast.error(data);
+			}
+			break;
+		case 401:
+			toast.error('Unauthorised');
+			break;
+		case 404:
+			history.push('/not-found');
+			break;
+		case 500:
+			store.commonStore.setServerError(data);
+			history.push('/server-error');
+			break;
+	}
+};
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-  get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-  post: <T>(url: string, body: {}) =>
-    axios.post<T>(url, body).then(responseBody),
-  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
-  del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+	get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+	post: <T>(url: string, body: {}) =>
+		axios.post<T>(url, body).then(responseBody),
+	put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+	del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
 
 const Activities = {
-  list: () => requests.get<Activity[]>("/activities"),
-  details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-  create: (activity: Activity) => requests.post("/activities", activity),
-  update: (activity: Activity) =>
-    requests.put(`/activities/${activity.id}`, activity),
-  delete: (id: string) => requests.del(`/activities/${id}`),
+	list: () => requests.get<Activity[]>('/activities'),
+	details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+	create: (activity: Activity) => requests.post('/activities', activity),
+	update: (activity: Activity) =>
+		requests.put(`/activities/${activity.id}`, activity),
+	delete: (id: string) => requests.del(`/activities/${id}`),
 };
 
 const agent = {
-  Activities,
+	Activities,
 };
 
 export default agent;
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-calendar/dist/Calendar.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history'
+
+export const history = createBrowserHistory();
 
 ReactDOM.render(
-	<BrowserRouter>
+	<Router history={history}>
 		<App />
-	</BrowserRouter>,
+	</Router>,
 	document.getElementById('root')
 );
 
