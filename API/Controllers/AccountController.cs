e086150dc454590530bd48a2a21a4a5cd47d6cc7
Kevin Hu <kevin.hu@purple.telstra.com>
2022-06-03 20:58:56 +1000

    End of Section 12: Add API layer authentication

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,94 @@
+using System.Security.Claims;
+using API.DTOs;
+using API.Services;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signinManager;
+        private readonly TokenService _tokenService;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signinManager, TokenService tokenService)
+        {
+            _signinManager = signinManager;
+            _tokenService = tokenService;
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized();
+
+            var result = await _signinManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (result.Succeeded)
+            {
+                return CreateUserObject(user);
+            }
+
+            return Unauthorized();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+            {
+                return BadRequest("Email token");
+            }
+
+            if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName))
+            {
+                return BadRequest("Username token");
+            }
+
+            var appUser = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.UserName
+            };
+
+            var result = await _userManager.CreateAsync(appUser, "Pa$$w0rd");
+
+            if (result.Succeeded)
+            {
+                return CreateUserObject(appUser);
+            }
+
+            return BadRequest("Errors occurred while registering user!");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return CreateUserObject(user);
+        }
+        private UserDto CreateUserObject(AppUser appUser)
+        {
+            return new UserDto
+            {
+                DisplayName = appUser.DisplayName,
+                Image = null,
+                Token = _tokenService.CreateToken(appUser),
+                UserName = appUser.UserName
+            };
+        }
+    }
+}
