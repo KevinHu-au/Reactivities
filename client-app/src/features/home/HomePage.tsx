aa6d9e75e8bbfc6a0ecc3c30d05a2c1b38ffde1c
Kevin Hu <kevin.hu@purple.telstra.com>
2022-06-04 18:52:56 +1000

    Add model for Login Form

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Header, Segment, Image, Button } from 'semantic-ui-react';
 import { useStore } from '../../app/stores/store';
+import LoginForm from '../users/LoginForm';
 
 export default observer(function HomePage() {
-	const { userStore } = useStore();
+	const { userStore, modalStore } = useStore();
 	return (
 		<Segment inverted textAlign='center' vertical className='masthead'>
 			<Container text>
@@ -21,12 +22,32 @@ export default observer(function HomePage() {
 				{userStore.isLoggedIn ? (
 					<>
 						<Header as='h2' inverted content='Welcome to Reactivities' />
-						<Button as={Link} to='/activities' content='Go to activities' size='huge' inverted />
+						<Button
+							as={Link}
+							to='/activities'
+							content='Go to activities'
+							size='huge'
+							inverted
+						/>
 					</>
 				) : (
-					<Button as={Link} to='/login' size='huge' inverted>
-						Login!
-					</Button>
+					<>
+						<Button
+							onClick={() => modalStore.openModal(<LoginForm />)}
+							size='huge'
+							inverted
+						>
+							Login!
+						</Button>
+
+						<Button
+							onClick={() => modalStore.closeModal()}
+							size='huge'
+							inverted
+						>
+							Register!
+						</Button>
+					</>
 				)}
 			</Container>
 		</Segment>
