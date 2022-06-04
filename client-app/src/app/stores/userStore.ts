ce6fd74e7d7629f5639e50968e49737e0d3dee90
Kevin Hu <kevin.hu@purple.telstra.com>
2022-06-04 15:40:02 +1000

    Persist the token in common store

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -31,4 +31,13 @@ export default class UserStore {
 		this.user = null;
 		history.push('/');
 	}
+
+	getUser = async() => {
+		try {
+			var user = await agent.Account.current();
+			runInAction(() => this.user = user);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 }
