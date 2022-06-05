061f51bf5a0874699781ab833a1dd1c8a28880ed
Kevin Hu <kevin.hu@purple.telstra.com>
2022-06-05 16:52:32 +1000

    Add activity and user m-to-m releationship with ActivityAttendees table

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -4,12 +4,31 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-  public class DataContext : IdentityDbContext<AppUser>
-  {
-    public DataContext(DbContextOptions options) : base(options)
+    public class DataContext : IdentityDbContext<AppUser>
     {
-    }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
 
-    public DbSet<Activity> Activities { get; set; }
-  }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
+
+            builder
+              .Entity<ActivityAttendee>()
+              .HasOne(aa => aa.AppUser)
+              .WithMany(appUser => appUser.Activities)
+              .HasForeignKey(aa => aa.AppUserId);
+
+            builder
+              .Entity<ActivityAttendee>()
+              .HasOne(aa => aa.Activity)
+              .WithMany(activity => activity.Attendees)
+              .HasForeignKey(aa => aa.ActivityId);
+        }
+    }
 }
