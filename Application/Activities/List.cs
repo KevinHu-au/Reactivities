acf8df072f38ede91cb3335a1f077ee06d673f96
Kevin Hu <kevin.hu@purple.telstra.com>
2022-06-05 20:39:35 +1000

    Use AutoMapper queryable projection to optimisation the EF call

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,6 +1,7 @@
 
 using Application.Core;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -27,8 +28,7 @@ namespace Application.Activities
             public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
-                    .Include(a => a.Attendees)
-                    .ThenInclude(aa => aa.AppUser)
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
                     .ToListAsync();
 
                 var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
